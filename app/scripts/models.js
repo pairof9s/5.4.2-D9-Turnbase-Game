10305fc012075198f7e7370cf0311b758de243fb
David Nine <pairof9s@me.com>
2016-07-24 19:40:44 -0400

    minor cleanup edits

--- app/scripts/models.js
+++ app/scripts/models.js
@@ -33,39 +33,3 @@ Enemy.prototype = new Dancer();
    'Hero': Hero,
    'Enemy': Enemy
 };
-
-
-// Code from Kurt whose game is like ours...
-
-// function Fighters(config){
-//  config = config || {};
-//  $.extend(this, config);
-//  this.health = config.health || 60;
-//  this.name = config.name;
-//  this.image = config.image;
-//  //this.battleCry = config.battleCry;
-// }
-//
-// Fighters.prototype.attack = function(victim) {
-//  var damage = _.random(20);
-//  victim.health = victim.health - damage;
-// };
-//
-//
-// function Hero(config){
-//  Fighters.call(this, config);
-// }
-//
-// Hero.prototype = new Fighters();
-//
-// function Enemy(config){
-//  Fighters.call(this, config);
-// }
-//
-// Enemy.prototype = new Fighters();
-//
-//
-// module.exports = {
-//  'Hero': Hero,
-//  'Enemy': Enemy
-// };
